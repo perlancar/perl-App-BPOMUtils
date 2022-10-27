a27cde5010fc98aad0098aeb9506b6538549c371
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2022-10-27 17:33:34 +0700

    Update table metadata, [ux] Add examples

--- lib/App/BPOMUtils.pm
+++ lib/App/BPOMUtils.pm
@@ -40,6 +40,30 @@ $res = gen_read_table_func(
 _
     extra_props => {
         examples => [
+            {
+                summary => 'Print active categories codes matching "cokelat"',
+                src_plang => "bash",
+                src => "[[prog]] --status-is Aktif 'cokelat hitam'",
+                test => 0,
+            },
+            {
+                summary => 'Print active records matching "cokelat hitam" in a formatted text table',
+                src_plang => "bash",
+                src => "[[prog]] --status-is Aktif 'cokelat hitam' -l --format text-pretty",
+                test => 0,
+            },
+            {
+                summary => 'Print all category records with code 14.1.4.2',
+                src_plang => "bash",
+                src => "[[prog]] --code-matches '^14010402' -l --format text-pretty",
+                test => 0,
+            },
+            {
+                summary => 'How many categories are active vs inactive?',
+                src_plang => "bash",
+                src => "echo -n 'Aktif: '; [[prog]] --status-is Aktif | wc -l; echo -n 'Tidak Aktif: '; [[prog]] --status-isnt Aktif | wc -l",
+                test => 0,
+            },
         ],
     },
 );
